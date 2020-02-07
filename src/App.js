1c2ec0e41c518c786edbff4fe3fe226cf1f360ca
Andrew Truex <andrewtruex@gmail.com>
2020-02-07 15:02:49 -0500

    Creating a Search Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react"
 import { render } from "react-dom"
-import Pet from "./Pet"
 import SearchParams from "./SearchParams"
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt me!</h1>
-      <Pet name="Scout" animal="Dog" breed="Beagle" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sparky" animal="Crab" breed="Hermit" />
       <SearchParams />
     </div>
   )
